6aebdcff52eefe4bd814e5b00890d9daf38969e1
zev <alfabetrayer@gmail.com>
2023-05-23 18:26:43 +0300

    дорабатываю вёрстку

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = navMain.querySelector('.main-nav__toggle');
+const logoLink = document.querySelector ('.main-header__logo-link');
 
 navMain.classList.remove('main-nav--nojs');
+logoLink.classList.remove('main-header__logo-link--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
